bb7916332e186336ab822571ca04c773cb8d9e52
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2026-06-03 17:39:57 +0100

    POPPM-131: Add insurance on-site check-in screen

--- popp-demo/popp-insurance-app-demo/sharedInsuranceApp/src/commonMain/kotlin/de/servicehealth/poppmodule/demo/insurance/App.kt
+++ popp-demo/popp-insurance-app-demo/sharedInsuranceApp/src/commonMain/kotlin/de/servicehealth/poppmodule/demo/insurance/App.kt
@@ -2,6 +2,10 @@ package de.servicehealth.poppmodule.demo.insurance
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import de.servicehealth.poppmodule.demo.BrandShowcaseScreen
 import de.servicehealth.poppmodule.demo.LocalPoppSdk
@@ -11,6 +15,15 @@ import de.servicehealth.poppmodule.sdk.PoppSdk
 @Preview
 fun App() {
     CompositionLocalProvider(LocalPoppSdk provides PoppSdk()) {
-        BrandShowcaseScreen()
+        var showCheckIn by remember { mutableStateOf(false) }
+        if (showCheckIn) {
+            OnsiteCheckInEntryScreen(
+                onClose = { showCheckIn = false },
+                onSearchClick = {},
+                onQrScanClick = {},
+            )
+        } else {
+            BrandShowcaseScreen(onOpenCheckIn = { showCheckIn = true })
+        }
     }
 }
